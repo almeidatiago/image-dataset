31ffd870e668187a1e520c27d60f382581fa2e00
almeidatiago <almiedatiago0729@gmail.com>
2022-09-21 16:52:48 -0300

    first commit

---
+++ gittopng.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from PIL import Image, ImageDraw, ImageFont #dynamic import
+
+import glob
+import os
+
+onlyGifs = glob.glob("*.tiff")
+
+size = 512, 512
+
+for i in onlyGifs:
+  img = Image.open(i).convert('L')
+  img.thumbnail(size, Image.ANTIALIAS)
+  img.save(os.path.splitext(i)[0] + ".png",'png', optimize=True, quality=70)
